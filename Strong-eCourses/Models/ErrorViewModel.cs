fb13b628801f8ce10e89b957c63ea1df993bd943
Lule-M <miha.luka1006@gmail.com>
2025-04-28 14:54:24 +0200

    again deleted project

--- Strong-eCourses/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Strong_eCourses.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
