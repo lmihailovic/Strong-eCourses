f7a9c9614ee5b126cd7dcc096e0c55f116a7f955
Luka Obradović <lukaobradovic5@gmail.com>
2025-05-15 02:06:27 +0200

    updated mongodbconfig class

--- Strong-eCourses/Settings/MongoDbConfig.cs
+++ Strong-eCourses/Settings/MongoDbConfig.cs
@@ -2,5 +2,8 @@
 
 public class MongoDbConfig
 {
-
+    public string Name {get; init;}
+    public string Host  {get; init;}
+    public int Port{get; init;}
+    public string ConnectionString => $"mongodb://{Host}:{Port}";
 }
