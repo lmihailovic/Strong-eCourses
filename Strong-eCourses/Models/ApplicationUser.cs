aa0bb798bb25079f9bb8b8db5c11db06bcc76274
Luka Obradović <lukaobradovic5@gmail.com>
2025-05-15 02:24:18 +0200

    altered ApplicationUser class

--- Strong-eCourses/Models/ApplicationUser.cs
+++ Strong-eCourses/Models/ApplicationUser.cs
@@ -1,6 +1,8 @@
-﻿namespace Strong_eCourses;
-
-public class ApplicationUser
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+namespace Strong_eCourses;
+[CollectionName("Users")]
+public class ApplicationUser:MongoIdentityUser<Guid>
 {
 
 }
