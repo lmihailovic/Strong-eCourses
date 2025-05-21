f845138993a45a0d41f80f140d2a5a0a65926460
Luka Obradović <lukaobradovic5@gmail.com>
2025-05-21 21:21:08 +0200

    added users to admin panel

--- Strong-eCourses/Controllers/AdminController.cs
+++ Strong-eCourses/Controllers/AdminController.cs
@@ -1,14 +1,32 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Strong_eCourses.Models;
 
 namespace Strong_eCourses.Controllers;
-[Authorize(Roles ="Admin")]
+
+[Authorize(Roles = "Admin")]
 public class AdminController : Controller
 {
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly RoleManager<ApplicationRole> _roleManager;
+    public AdminController(UserManager<ApplicationUser> userManager,RoleManager<ApplicationRole> roleManager)
+    {
+
+        _userManager = userManager;
+        _roleManager = roleManager;
+
+    }
+    [Authorize(Roles = "Admin")]
     public IActionResult Admin()
     {
-        return View();
+        var users = _userManager.Users.ToList();
+        return View(users);
     }
+    
+
 }
+
+
+
