9221339184fd2536ca7a000e5dd1c64f947dc517
Luka Obradović <lukaobradovic5@gmail.com>
2025-05-29 03:00:17 +0200

    added insert option, update option not working

--- Strong-eCourses/Models/Course.cs
+++ Strong-eCourses/Models/Course.cs
@@ -7,7 +7,7 @@ public class Course
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [BsonElement("name")]
     public string Name { get; set; }
